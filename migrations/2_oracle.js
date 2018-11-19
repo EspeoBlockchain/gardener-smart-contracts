d3a93f91d3ad36c0773d5c44b66ec670e278deac
krzysztofwedrowicz <krzysztof.wedrowicz@espeo.eu>
2018-11-19 21:02:29 +0100

    added env variables, improved migrations, support for ropsten

--- migrations/2_oracle.js
+++ migrations/2_oracle.js
@@ -1,5 +1,5 @@
 const Oracle = artifacts.require('Oracle.sol');
 
-module.exports = (deployer, network, accounts) => {
-  deployer.deploy(Oracle, accounts[9]);
+module.exports = (deployer) => {
+  deployer.deploy(Oracle, process.env.ORACLE_SERVER_ADDRESS);
 };
--- truffle.js
+++ truffle.js
@@ -1,3 +1,5 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+require('dotenv').config();
 /*
 § * NB: since truffle-hdwallet-provider 0.0.5 you must wrap HDWallet providers in a
  * function when declaring them. Failure to do so will cause commands to hang. ex:
@@ -12,6 +14,7 @@
  *   },
  */
 
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
@@ -20,6 +23,11 @@ module.exports = {
       host: '127.0.0.1',
       port: 8545,
       network_id: '*', // match any network
+    },
+    ropsten: {
+      // eslint-disable-next-line max-len
+      provider: () => new HDWalletProvider(process.env.PRIVATE_KEY || process.env.MNEMONIC, process.env.ROPSTEN_PROVIDER_URL),
+      network_id: '3',
     },
   },
 };
