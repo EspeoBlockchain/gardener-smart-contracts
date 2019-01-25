8dbdaeeccbdb9852e69e1d22b9e9c366a3456f2f
krzysztofwedrowicz <krzysztof.wedrowicz@espeo.eu>
2019-01-25 15:49:27 +0100

    fixed solidity-coverage, downgraded solidity to 0.5.0 from 0.5.3

--- contracts/OracleI.sol
+++ contracts/OracleI.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.3;
+pragma solidity ^0.5.0;
 
 interface OracleI {
     function request(string calldata _url) external returns(bytes32 id);
--- contracts/UsingOracleI.sol
+++ contracts/UsingOracleI.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.3;
+pragma solidity ^0.5.0;
 
 interface UsingOracleI {
     function __callback(bytes32 _id, string calldata _value, uint _errorCode) external;
